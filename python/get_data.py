dcfec766252c7aeee328150e44c05052485deb51
kiankd <kian.kenyon-dean@mail.mcgill.ca>
2015-11-25 19:04:16 -0500

    using npy files

--- python/get_data.py
+++ python/get_data.py
@@ -1,5 +1,5 @@
 import numpy as np
 
 def get_data():
-    return np.load('../rotated_data_features'),np.load('../y_classifications')
+    return np.load('../rotated_features'),np.load('../y_classifications')
 
