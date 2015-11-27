a6cadd01b3e7046762a01673b760f8b6d8955b90
Alan <test@example.com>
2015-11-27 16:18:39 -0500

    Changed the code for generating confusion matrices.

--- baseline_results/plot_cm.py
+++ baseline_results/plot_cm.py
@@ -1,13 +1,23 @@
+# Author: Alan Do-Omri
+
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.preprocessing import normalize
 
 for string in ['GaussianNB', 'LinearSVC', 'LogisticRegression', 'SVC']:
+# for string in ['GaussianNB']:
 	cm = np.load('%s_confusion_matrix.npy' % string)
 	cm = cm.astype('float')/cm.sum(axis=1)
 
 	plt.matshow(cm, cmap=plt.cm.Greens)
 	plt.title('Confusion Matrix for %s' % string)
 	plt.colorbar()
-	plt.tick_params(bottom=True)
+
+	ax = plt.gca()
+	ax.xaxis.set_ticks_position('bottom')
+
+	for x in range(15):
+		for y in range(15):
+			if cm[x,y]>=0.15:
+				ax.annotate('%4.2f' % cm[x,y], xy=(y,x), horizontalalignment='center', verticalalignment='center', size=6.3)
+
 	plt.savefig('%s_cm.pdf' % string)
