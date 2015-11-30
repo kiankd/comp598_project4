7121cfce00097d4caa4ff4c54a441bc2b1289467
kiankd <kian.kenyon-dean@mail.mcgill.ca>
2015-11-30 15:29:30 -0500

    added lbp classification functionality

--- python/baseline_classification.py
+++ python/baseline_classification.py
@@ -7,36 +7,44 @@ from get_data import get_train_val_test
 from sklearn.svm import SVC, LinearSVC
 from sklearn.naive_bayes import GaussianNB
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import f1_score, confusion_matrix
+from sklearn.metrics import f1_score, confusion_matrix, classification_report
 
 def test_classifier(model, X, y, testX, testy):
     print 'Fitting model %s...'%(type(model).__name__)
     model.fit(X,y)
     yhat = model.predict(testX)
-    return f1_score(testy, yhat), confusion_matrix(testy, yhat)
+    return f1_score(testy, yhat), confusion_matrix(testy, yhat), classification_report(testy, yhat)
 
 if __name__ == '__main__':
-    trainX,trainy, valX,valy, testX,testy = get_train_val_test()
+    data_directory = '../sanctuary/lbp_dataset_2_8/'
+    #trainX,trainy, valX,valy, testX,testy = get_train_val_test()
+    trainX = np.load(data_directory + 'lbp_trainX.npy')
+    trainy = np.load(data_directory + 'lbp_trainY.npy')
+    testX = np.load(data_directory + 'lbp_testX.npy')
+    testy = np.load(data_directory + 'lbp_testY.npy')
     
     trainX = normalize(trainX)
-    valX = normalize(valX)
+    #valX = normalize(valX)
     testX = normalize(testX)
 
     names = []
     scores = []
     matrices = []
+    reports = []
 
     for model_class in [LinearSVC, GaussianNB, SVC, LogisticRegression]:
         model = model_class()
         print model
         names.append(type(model).__name__)
-        f1, conf_mat = test_classifier(model, trainX, trainy, testX, testy)
+        f1, conf_mat, report = test_classifier(model, trainX, trainy, testX, testy)
         print 'Score: ',f1
         scores.append(f1)
         matrices.append(conf_mat)
+        reports.append(report)
 
-    with open('../baseline_results.txt','w') as f:
+    with open('../lbp_baseline_results.txt','w') as f:
         for i in range(len(names)):
             f.write('%s - f1_score: %0.3f\n'%(names[i], scores[i]))
-            np.save('../%s_confusion_matrix'%names[i], matrices[i])
+            np.save('../lbp_%s_confusion_matrix'%names[i], matrices[i])
+            f.write('Classification Report:\n%s\n'%reports[i])
 
