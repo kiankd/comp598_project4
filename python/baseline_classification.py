ba233bb66c3918f24d04d64daf6ac6e8d1710327
kiankd <kian.kenyon-dean@mail.mcgill.ca>
2015-12-01 14:43:40 -0500

    simple change

--- python/baseline_classification.py
+++ python/baseline_classification.py
@@ -17,11 +17,15 @@ def test_classifier(model, X, y, testX, testy):
 
 if __name__ == '__main__':
     data_directory = '../sanctuary/lbp_dataset_2_8/'
-    #trainX,trainy, valX,valy, testX,testy = get_train_val_test()
-    trainX = np.load(data_directory + 'lbp_trainX.npy')
-    trainy = np.load(data_directory + 'lbp_trainY.npy')
-    testX = np.load(data_directory + 'lbp_testX.npy')
-    testy = np.load(data_directory + 'lbp_testY.npy')
+    
+    prefix = 'new_normalize'
+    if prefix != 'lbp':
+        trainX,trainy, valX,valy, testX,testy = get_train_val_test()
+    else:
+        trainX = np.load(data_directory + 'lbp_trainX.npy')
+        trainy = np.load(data_directory + 'lbp_trainY.npy')
+        testX = np.load(data_directory + 'lbp_testX.npy')
+        testy = np.load(data_directory + 'lbp_testY.npy')
     
     trainX = normalize(trainX)
     #valX = normalize(valX)
@@ -42,9 +46,9 @@ if __name__ == '__main__':
         matrices.append(conf_mat)
         reports.append(report)
 
-    with open('../lbp_baseline_results.txt','w') as f:
+    with open('../'+prefix+'_baseline_results.txt','w') as f:
         for i in range(len(names)):
             f.write('%s - f1_score: %0.3f\n'%(names[i], scores[i]))
-            np.save('../lbp_%s_confusion_matrix'%names[i], matrices[i])
+            np.save('../'+prefix+'_%s_confusion_matrix'%names[i], matrices[i])
             f.write('Classification Report:\n%s\n'%reports[i])
 
